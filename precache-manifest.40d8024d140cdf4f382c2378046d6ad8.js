85f9300a15fda75213dab0742e0e6c2bb56028c0
alimahmudlu <alimahmudlu@gmail.com>
2020-06-04 22:24:38 +0400

    Updates

---
+++ precache-manifest.40d8024d140cdf4f382c2378046d6ad8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bee67a2c5cbf884e04aa0cc26f29e9",
+    "url": "/react_admin/index.html"
+  },
+  {
+    "revision": "06b08f58a7af031714c2",
+    "url": "/react_admin/static/css/2.eee5d9d8.chunk.css"
+  },
+  {
+    "revision": "4bfc141b7b4c99929cda",
+    "url": "/react_admin/static/css/main.f88fac26.chunk.css"
+  },
+  {
+    "revision": "06b08f58a7af031714c2",
+    "url": "/react_admin/static/js/2.cdfb74d2.chunk.js"
+  },
+  {
+    "revision": "9303ee64f2b3242139bf3d97728ac05a",
+    "url": "/react_admin/static/js/2.cdfb74d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfc141b7b4c99929cda",
+    "url": "/react_admin/static/js/main.946b9749.chunk.js"
+  },
+  {
+    "revision": "ebbf631c44a4497b2ab5",
+    "url": "/react_admin/static/js/runtime-main.b0e92717.js"
+  },
+  {
+    "revision": "976127e176ef117b321da475bd25e433",
+    "url": "/react_admin/static/media/defaultimg.976127e1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7fe0544da18603b4e3a0c1089ace55f3",
+    "url": "/react_admin/static/media/person.7fe0544d.jpg"
+  }
+]);
