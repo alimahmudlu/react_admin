323689a1e441e59dc0475db04c88a0e3f0ffa920
alimahmudlu <alimahmudlu@gmail.com>
2020-06-21 16:47:34 +0400

    add breadcrumb

--- src/Components/Content/Breadcrumb.js
+++ src/Components/Content/Breadcrumb.js
@@ -1,116 +1,16 @@
 import React from "react";
-import "./Content.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome} from "@fortawesome/free-solid-svg-icons";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import withBreadcrumbs from "react-router-breadcrumbs-hoc";
 
+const PureBreadcrumbs = ({ breadcrumbs }) => (
+    <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+        {breadcrumbs.map(({ breadcrumb, match }, index) => (
+            <Link className="breadcrumb-item" to={match.url || ""}>{breadcrumb}</Link>
+        ))}
+        </ol>
+    </nav>
+);
 
+export default withBreadcrumbs()(PureBreadcrumbs);
 
-
-export default function Breadcrumb() {
-
-    const pathname = window.location.pathname;
-    const arr = pathname.split("/");
-    const arr2 = arr.shift();
-
-    return (
-        <>
-{/*            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <Link to="/" className="breadcrumb-item">
-                        <FontAwesomeIcon icon={faHome} className="icon breadcrumb-item--icon"/>
-                        Home
-                    </Link>
-                    {arr.length === 0 ? " aaa" :
-                        <>
-                            {arr.map((item, index) => {
-                                return (
-                                    <>
-                                        <li className="breadcrumb-item" key={index}>
-                                            <Link to={["/",item].join("")}>
-                                                {item}
-                                            </Link>
-                                        </li>
-                                    </>
-                                )
-                            })}
-                        </>
-                    }
-                </ol>
-            </nav>*/}
-        </>
-    )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import React from "react";
-import "./Content.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome} from "@fortawesome/free-solid-svg-icons";
-import {Link} from "react-router-dom";
-
-
-export default function Breadcrumb(props) {
-    const {breadcrumb_childitem, breadcrumb_activeitem} = props;
-    return (
-        <>
-            {/!*<nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <Link to="/" className="breadcrumb-item">
-                        <FontAwesomeIcon icon={faHome} className="icon breadcrumb-item--icon"/>
-                        Home
-                    </Link>
-                    {breadcrumb_childitem === "" ? "" :
-                        <>
-                            {breadcrumb_childitem.map((item) => {
-                                return (
-                                    <>
-                                        <li className="breadcrumb-item" aria-current="page">
-                                            <Link to={item.page_link}>
-                                                {item.page_name}
-                                            </Link>
-                                        </li>
-                                    </>
-                                )
-                            })}
-                        </>
-                    }
-
-                    {breadcrumb_activeitem.length === 0 ? "" :
-                        <li className="breadcrumb-item" aria-current="page">
-                            {breadcrumb_activeitem}
-                        </li>
-                    }
-                </ol>
-            </nav>*!/}
-        </>
-    )
-}
-*/
